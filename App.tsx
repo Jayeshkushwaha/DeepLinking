24b05750cbc1308092b34189a63c8a8dea7a33e3
Jayesh Kushwaha <95064038+Jayeshkushwaha@users.noreply.github.com>
2024-04-21 19:11:13 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -20,10 +20,6 @@ import { Image, StyleSheet, ActivityIndicator } from 'react-native';
 const Tab = createMaterialBottomTabNavigator();
 
 const Stack = createNativeStackNavigator();
-const HomeStack = createNativeStackNavigator();
-const FavStack = createNativeStackNavigator();
-const ProfileStack = createNativeStackNavigator();
-const SettingsStack = createNativeStackNavigator();
 
 const linking = {
   prefixes: ['demoapp://'],
